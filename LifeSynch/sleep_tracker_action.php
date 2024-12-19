78f299ecaed1aa5620fb9e7d590b78b6cabefa1a
Divine igirubuntu <divineigirubuntu@Divines-MacBook-Air.local>
2024-12-19 21:04:06 +0000

    change

--- LifeSynch/sleep_tracker_action.php
+++ LifeSynch/sleep_tracker_action.php
@@ -5,7 +5,6 @@ function saveSleepAndWakeTime(user) {
     var sleepTime = document.getElementById('sleep-time').value;
     var wakeTime = document.getElementById('wake-time').value;
 
-    // Check if both sleep and wake times are entered
     if (sleepTime && wakeTime) {
         var userId = user;
         var xhr = new XMLHttpRequest();
@@ -14,13 +13,11 @@ function saveSleepAndWakeTime(user) {
         
         xhr.onload = function() {
             var response = JSON.parse(xhr.responseText);
-            alert(response.message);  // Show response message
+            alert(response.message);
         };
 
-        // Send both sleep and wake time together
         xhr.send('user_id=' + userId + '&sleep_time=' + sleepTime + '&wake_time=' + wakeTime);
     } else {
-        // Alert if either sleep or wake time is missing
         alert("Please enter both sleep time and wake time.");
     }
 }
