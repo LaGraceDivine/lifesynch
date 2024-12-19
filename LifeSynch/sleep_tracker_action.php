b3c94ecb7d41e7d0bdb215636a880feec3daa38d
Divine igirubuntu <divineigirubuntu@Divines-MacBook-Air.local>
2024-12-19 20:53:36 +0000

    change

--- LifeSynch/sleep_tracker_action.php
+++ LifeSynch/sleep_tracker_action.php
@@ -1,22 +1,27 @@
 <?php
 include('dbConnect.php');
 
-if (isset($_POST['user_id']) && isset($_POST['sleep_time'])) {
+if (isset($_POST['user_id']) && isset($_POST['sleep_time']) && isset($_POST['wake_time'])) {
     $user_id = $_POST['user_id'];
     $sleep_time = $_POST['sleep_time'];
+    $wake_time = $_POST['wake_time'];
+
+
     $sleep_time = str_replace("T", " ", $sleep_time) . ":00";
+    $wake_time = str_replace("T", " ", $wake_time) . ":00";
+
     $created_at = date('Y-m-d H:i:s');
+
     $query = "INSERT INTO sleep_tracker (user_id, sleep_time, wake_time, created_at) 
-              VALUES (?, ?, ?,?)";
+              VALUES (?, ?, ?, ?)";
     if ($stmt = mysqli_prepare($conn, $query)) {
         
-        mysqli_stmt_bind_param($stmt, "iss", $user_id, $sleep_time, $created_at);
+
+        mysqli_stmt_bind_param($stmt, "isss", $user_id, $sleep_time, $wake_time, $created_at);
 
         if (mysqli_stmt_execute($stmt)) {
-          
             echo json_encode(['status' => 'success', 'message' => 'Sleep time recorded']);
         } else {
-        
             echo json_encode(['status' => 'error', 'message' => mysqli_error($conn)]);
         }
 
@@ -25,11 +30,9 @@ if (isset($_POST['user_id']) && isset($_POST['sleep_time'])) {
         echo json_encode(['status' => 'error', 'message' => 'Failed to prepare the query']);
     }
 } else {
-    
     echo json_encode(['status' => 'error', 'message' => 'Missing data']);
 }
-
-
 ?>
+
 
 
